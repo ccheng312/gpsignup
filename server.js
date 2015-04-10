f0a254b6adef7dd490bb1f7ad6b4ba6adf825cce
Michael Shuh <michael.shuh@gmail.com>
2015-04-10 16:35:46 -0500

    connect to mongoose and mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -10,6 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+
+// Configure mongoose to connect to mongodb
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log("Connection success to DB");
+});
 
 
 // START THE SERVER
