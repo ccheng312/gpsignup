614121529c9c723cd65131d49f4212b901ac7518
cchengbv <chris.cheng@bazaarvoice.com>
2015-04-10 16:10:01 -0500

    Initial commit of project structure

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Started server on port ' + port);
