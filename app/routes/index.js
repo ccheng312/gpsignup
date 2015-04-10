dee01f33c983ac07fcd14327a6e0850ecb25eb9e
Michael Shuh <michael.shuh@gmail.com>
2015-04-10 17:09:36 -0500

    adding a main route in case we need it

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
+    app.use('/api', require('./main.routes'));
     app.use('/api', require('./events.routes'));
     app.use('/api', require('./slots.routes'));
 };
