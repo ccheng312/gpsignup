08934886e07cf02010b1aba77653c678ce4d1fb1
Michael Shuh <michael.shuh@gmail.com>
2015-04-13 23:00:31 -0500

    use colons instead of equal sign

--- app/models/slots.model.js
+++ app/models/slots.model.js
@@ -13,8 +13,8 @@ var SlotSchema = new Schema({
     start_time: { type: Date },
     end_time: { type: Date },
     capacity: { type: Number, min: 0 },
-    quantity = { type: Number, min: 0 },
-    enabled = { type: Boolean }
+    quantity: { type: Number, min: 0 },
+    enabled: { type: Boolean }
     // Need some foreign key into the event it's part of and the location
 
 });
