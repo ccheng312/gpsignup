ebd4ce77292ee549bf96424809ff2d5c350a1919
Michael Shuh <michael.shuh@gmail.com>
2015-04-13 22:56:58 -0500

    adding slot api call

--- app/controllers/slots.controller.js
+++ app/controllers/slots.controller.js
@@ -3,18 +3,77 @@
 /**
  * Module Dependencies
  */
-
+var mongoose = require('mongoose'),
+    Slot = mongoose.model('Slot'),
+    _ = require('lodash');
 
 
 /**
  * Slot Methods
  */
 exports.create = function(req, res) {
-    // create a new slot in db
-    res.status(501).send({ error: 'not implemented' });
+    var slot = new Slot(req.body);
+
+    // save the slot and check for errors
+    slot.save(function(err) {
+        if (err) {
+            // TODO: make sure this works as expected.
+            return res.send(err);
+        }
+
+        res.json({ message: 'Slot created!' });
+    });
 };
 
 exports.list = function(req, res) {
     // grab list of slots from db
-    res.status(501).send({ error: 'not implemented' });
+    Slot.find(function(err, slots) {
+        if (err) {
+            return res.send(err);
+        }
+
+        res.json(slots);
+    });
+};
+
+exports.read = function(req, res) {
+    res.json(req.slot);
+};
+
+exports.update = function(req, res) {
+    var slot = _.extend(req.slot, req.body);
+
+    slot.save(function(err) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json({ message: 'Slot updated!' });
+    });
+};
+
+exports.delete = function(req, res) {
+    var slot = req.slot;
+
+    slot.remove(function(err) {
+        if (err) {
+            return res.send(err);
+        }
+        res.json({ message: 'Slot deleted!' });
+    });
+};
+
+/**
+ * Middleware
+ */
+exports.slotById = function(req, res, next, id) {
+    Slot.findById(id).exec(function(err, slot) {
+        if (err) {
+            return next(err);
+        }
+        if (!slot) {
+            return next(new Error('Failed to load slot ' + id));
+        }
+        req.slot = slot;
+        next();
+    });
 };
--- app/models/slots.model.js
+++ app/models/slots.model.js
@@ -10,6 +10,12 @@ var mongoose = require('mongoose'),
  * Slot Schema
  */
 var SlotSchema = new Schema({
+    start_time: { type: Date },
+    end_time: { type: Date },
+    capacity: { type: Number, min: 0 },
+    quantity = { type: Number, min: 0 },
+    enabled = { type: Boolean }
+    // Need some foreign key into the event it's part of and the location
 
 });
 
