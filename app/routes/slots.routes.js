3247d0732119569c22dfcc25e351cf660b34c219
Michael Shuh <michael.shuh@gmail.com>
2015-04-13 22:57:37 -0500

    fix reference to event with slot

--- app/routes/slots.routes.js
+++ app/routes/slots.routes.js
@@ -16,4 +16,14 @@ router.route('/slots')
     .get(slots.list)
     .post(slots.create);
 
+router.route('/slots/:slotId')
+    // get the slot with that id (accessed at GET /api/slots/:slotId)
+    .get(slots.read)
+    // update the slot with that id (accessed at PUT /api/slots/:slotId)
+    .put(slots.update)
+    .delete(slots.delete);
+
+// This middleware makes the event available as req.signupEvent when you supply eventId
+router.param('slotId', slots.slotById);
+
 module.exports = router;
