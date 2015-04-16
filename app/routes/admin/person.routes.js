a2035829b05519e3ee17d40b3c1a8246b3db5359
Michael Shuh <michael.shuh@gmail.com>
2015-04-16 16:12:12 -0500

    add admin routes

---
+++ app/routes/admin/person.routes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Module Dependencies
+ */
+var express = require('express');
+var people = require('../../controllers/people.controller');
+
+
+/**
+ * People Routes
+ */
+var router = express.Router();
+
+router.route('/people')
+    .post(people.create);
+
+
+router.route('/people/:personId')
+    // get the person with that id (accessed at GET /admin/people/:personId)
+    .get(people.read)
+    // update the person with that id (accessed at PUT /admin/people/:personId)
+    .put(people.update)
+
+// This middleware makes the person available as req.person when you supply personId
+router.param('personId', people.personById);
+
+module.exports = router;
