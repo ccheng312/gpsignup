d5c0e7405846b4ca8eb62d1ee5371c2b76be416a
cchengbv <chris.cheng@bazaarvoice.com>
2015-04-15 21:15:34 -0500

    Add test framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,49 @@
-var gulp = require('gulp');
-var spawn = require('child_process').spawn;
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    spawn = require('child_process').spawn;
+    processes = [];
+
+function handleError(err) {
+    console.log(err.toString());
+}
+
+function shutdown() {
+    console.log('Killing processes...');
+    processes.forEach(function(proc) {
+        process.kill(proc.pid);
+    });
+    process.kill(process.pid);
+    console.log('Done!');
+}
 
 function runCommand(command, showOutput) {
   return function () {
     var args = command.split(' ');
     if (showOutput) {
-        spawn(args.shift(), args, { stdio: 'inherit' });
+        return processes.push(spawn(args.shift(), args, { stdio: 'inherit' }));
     } else {
-        spawn(args.shift(), args);
+        return processes.push(spawn(args.shift(), args));
     }
   }
 }
 
+gulp.task('env:test', function() {
+    console.log('Setting environment to test.');
+    process.env.NODE_ENV = 'test';
+});
+
+gulp.task('run-tests', ['start-mongo'], function() {
+    gulp.src(['app/test/*.test.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec',
+            timeout: 5000
+        }))
+        .on('error', handleError)
+        .on('end', shutdown);
+});
+
 gulp.task('start-mongo', runCommand('mongod --dbpath .db'));
 gulp.task('start-server', runCommand('nodemon --watch app/ server.js', true));
+gulp.task('test', ['env:test', 'start-mongo', 'run-tests']);
 
 gulp.task('default', ['start-mongo', 'start-server']);
