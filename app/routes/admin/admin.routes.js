e3c9c6ff700ba243c1095360c867afd9f8974d82
Michael Shuh <michael.shuh@gmail.com>
2015-04-16 19:46:45 -0500

    rename main to api/admin respectively, this allows them to get loaded first and we can do middleware for all api/admin calls

--- app/routes/admin/admin.routes.js
+++ app/routes/admin/admin.routes.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function (req,res,next) {
+    console.log("Admin stuff");
+});
+
 router.get('/', function(req, res) {
     res.json({ message: 'This is the main ADMIN location' });
 });
